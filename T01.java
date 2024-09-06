ded56300262946a37c301013b0a9eee621689312
IndahNainggolan <indahnnyxt@gmail.com>
2024-09-06 11:42:38 +0700

    Sudah selesai

--- T01.java
+++ T01.java
@@ -1,1 +1,27 @@
-// NIM - Your Name
+// 12S24046 - INDAH ELSADAI NAINGGOLAN
+// 12S24053 - DEA ANGGREANY HUTAPEA
+
+import java.util.*;
+import java.lang.Math;
+
+public class T01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN, jUDUL, pENULIS, pENERBIT, fORMAT;
+        int tAHUNTERBIT, sTOK;
+        double hARGA, mINIMUMMARGIN, rATING;
+
+        iSBN = input.nextLine();
+        jUDUL = input.nextLine();
+        pENULIS = input.nextLine();
+        pENERBIT = input.nextLine();
+        fORMAT = input.nextLine();
+        tAHUNTERBIT = Integer.parseInt(input.nextLine());
+        sTOK = Integer.parseInt(input.nextLine());
+        hARGA = Double.parseDouble(input.nextLine());
+        mINIMUMMARGIN = Double.parseDouble(input.nextLine());
+        rATING = Double.parseDouble(input.nextLine());
+        System.out.println(iSBN + "|" + jUDUL + "|" + pENULIS + "|" + pENERBIT + "|" + fORMAT + "|" + tAHUNTERBIT + "|" + sTOK + "|" + hARGA + "|" + mINIMUMMARGIN + "|" + rATING);
+    }
+}
